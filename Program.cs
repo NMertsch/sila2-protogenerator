6011cffdb642b32c0edc1c0a31279ccfbf37fceb
NMertsch <niklas.mertsch@stud.uni-goettingen.de>
2022-06-12 18:38:10 +0200

    basic program without cli wrapper and expecting xsl files in current directory

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml;
+using System.Xml.Xsl;
+
+var xslFile = "xsl/fdl2proto.xsl";
+var inputFile = args[0];
+var outputFile = args[1];
+
+var xslt = new XslCompiledTransform();
+xslt.Load(xslFile, new XsltSettings(), new XmlUrlResolver());
+xslt.Transform(inputFile, outputFile);
